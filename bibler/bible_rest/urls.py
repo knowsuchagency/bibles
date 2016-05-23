4a0248854a6946171d036c0a2347915db7f44083
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2016-05-22 23:06:56 -1000

    fixed urls.py and added pagination.

--- bibler/bible_rest/urls.py
+++ bibler/bible_rest/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    url(r"^verses/$", views.verse_list),
-    url(r"^verses/(?P<pk>[0-9]+)/$", views.verse_detail),
+    url(r"^verses/$", views.VerseList.as_view()),
+    url(r"^verses/(?P<pk>[0-9]+)/$", views.VerseDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- bibler/bibler/settings.py
+++ bibler/bibler/settings.py
@@ -124,6 +124,6 @@ STATIC_URL = '/static/'
 
 # rest_framework settings
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'apps.core.pagination.StandardResultsSetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
