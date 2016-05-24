4fabdc1a2ce4f165d3ecd375051105ac78b9a0c3
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2016-05-23 23:00:57 -1000

    updated settings

--- bibler/settings.py
+++ bibler/settings.py
@@ -92,10 +92,10 @@ DATABASES = {
     }
 }
 if not LOCAL_FLAG:
-    # Update database configuration with $DATABASE_URL.
     import dj_database_url
-    db_from_env = dj_database_url.config()
-    DATABASES['default'].update(db_from_env)
+    DATABASES['default'] = dj_database_url.config()
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    ALLOWED_HOSTS = ['*']
 
 
 
