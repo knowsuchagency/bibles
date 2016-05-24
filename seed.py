674955bb6ff004c1d39819a0fa4c8a09acbee177
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2016-05-23 18:02:10 -1000

    refactored app

--- seed.py
+++ seed.py
@@ -6,8 +6,6 @@ from tqdm import tqdm
 import django, os, sys, bibleparser
 
 # configure django so as to be able to use the models api
-bibler = Path('.', 'bibler')
-sys.path.append(str(bibler))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'bibler.settings'
 django.setup()
 
@@ -20,5 +18,4 @@ bible = bibleparser.Bible(path=bibles_path)
 for verse in tqdm(bible.verses):
     verse = verse._asdict()
     BibleVerse.objects.get_or_create(**verse)
-    # bible_verse = BibleVerse(**verse)
-    # bible_verse.save()
+
